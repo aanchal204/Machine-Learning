8afe7d9a1d2f652d35bfb142c9ebaf9828123475
Aanchal Dalmia <aanchal204@yahoo.in>
2016-08-26 18:23:15 +0530

    Solved Logistic regression

---
+++ Ex 02 Logistic Regression/ex2/sigmoid.m
@@ -0,0 +1,17 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+sig = @(x) (1.0 ./ (1 .+ e.^(-x)));
+g = sig(z);
+
+% =============================================================
+
+end
